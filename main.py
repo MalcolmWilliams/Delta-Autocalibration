30aa8357f01c001ca33ff27420e335a176942e09
Malcolm Williams <malcolmnwilliams@gmail.com>
2016-05-20 20:17:46 -0400

    start genetics

---
+++ main.py
@@ -0,0 +1,111 @@
+import PrinterModel
+import math
+
+
+'''
+basis of genetic algorithms
+
+have a population of best solutions
+
+Selection:
+	rank each solution based on its deviation from target	
+	higher ranked solutions have a better chance of being kept
+
+Crossover: 
+	avearge a random parameter between two solutions
+
+Mutation:
+	randomly vary a parameter
+
+'''
+
+class individual:
+	# a single member of the larger population
+
+	paramters = []
+
+	effectorPos = []
+
+	cost = 0	#correctness of the solution. this dictates which have a higher chance of being selected and crossed over. 
+	normalCost = 0	#normalised from 0 to 1
+
+	def setParameters(newParamters):
+		self.paramters = newParamters
+
+	def setEffectorPos(newEffectorPos):
+		self.effectorPos = newEffectorPos
+
+	def setCost(newCost):
+		self.cost = newCost
+
+	def getParamters():
+		return self.paramters
+
+	def getEffectorPos():
+		return self.effectorPos
+
+	def getCost():
+		return self.cost
+
+	def setNormalCost(maxCost):
+		self.normalCost = cost/maxCost
+
+
+def cost(testPos):
+	cost = 0
+	for i in range(3):
+		cost += (testPos - targetPos)**2
+	return math.sqrt(cost)
+
+def updateCosts():
+	maxCost = 0
+	for i in range(len(population)):
+		#calculates a new effector position and then the resulting cost.
+		printer.setParameters(population[i].getParamters())
+		population[i].setEffectorPos(printer.getEffectorPosition())
+		cost = cost(population[i].getEffectorPos)
+		population[i].setCost(cost)
+
+		if (maxCost < cost):
+			maxCost = cost
+
+	return maxCost
+
+def crossover(parameters1, parameters2):
+	#average a random parameter and append the new thing to the population array.
+	
+
+def geneticSelection():
+	readyForCrossover = False
+	crossoverParams = []
+	normalCost = 0
+	for i in range(10000):
+		maxCost = updateCosts()
+		for i in range(len(population)):
+			normalCost = population[i].getCost()/maxCost	#sets the normalised cost of the parameters, smaller is better, between 0 and 1
+
+			#do the genetic stuff
+			if (normalCost < selectionThreshold):
+				if (readyForCrossover):
+					#do crossover stuff
+					crossover(population[i].getParamters(), crossoverParams)
+					readyForCrossover = False
+				else:
+					readyForCrossover = True
+					crossoverIdx = i
+			else if (normalCost > rejectionThreshold):
+				#destroy the individual
+			#the ones between the two thresholds dont get destroyed, but also dont get to reproduce
+
+			#random mutation chance for each individual
+
+
+
+
+
+
+
+
+printer = PrinterModel.PrinterModel()
+print printer.getEffectorPosition()
+printer.exportVars()
--- test.py
+++
@@ -1,7 +0,0 @@
-import PrinterModel
-
-printer = PrinterModel.PrinterModel()
-
-
-print printer.getEffectorPosition()
-printer.exportVars()
