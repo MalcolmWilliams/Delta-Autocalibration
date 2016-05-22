131ea7c06d85f8e0b50390c305d8aa8b615009bc
Malcolm Williams <malcolmnwilliams@gmail.com>
2016-05-21 23:04:55 -0400

    time profiling
    also tested out effectiveness of "extinction events"

--- main.py
+++ main.py
@@ -48,143 +48,158 @@ class individual( object ):
 		return self.cost
 
 
-
-def calculateCost(testPos, targetPos):
-	cost = 0
-	for i in range(3):
-		cost += (testPos[i] - targetPos[i])**2
-	return cost
-
-def updateCosts(population, targetEffectorPos, travelDist):
-	for i in range(len(population)):
-		#calculates a new effector position and then the resulting cost.
-		printer.setParameters(population[i].getParameters())
+class GeneticOptimization( object ):
+	def calculateCost(self, testPos, targetPos):
+		'''
 		cost = 0
-		for j in range(len(travelDist)):	#targetEffectorPos is a 2d Array
-			printer.setParameters(travelDist[j])
-			population[i].setEffectorPos(printer.getEffectorPosition())
-			cost += calculateCost(population[i].getEffectorPos(), targetEffectorPos[j])
-		population[i].setCost(cost)
+		for i in range(3):
+			cost += (testPos[i] - targetPos[i])**2
+		return cost
+		'''
+		return (testPos[2]-targetPos)**2
+
+	def updateCosts(self, population, targetEffectorPos, travelDist, printer):
+		for i in range(len(population)):
+			#calculates a new effector position and then the resulting cost.
+			printer.setParameters(population[i].getParameters())
+			cost = 0
+			for j in range(len(travelDist)):	#targetEffectorPos is a 2d Array
+				printer.setParameters(travelDist[j])
+				population[i].setEffectorPos(printer.getEffectorPosition())
+				cost += self.calculateCost(population[i].getEffectorPos(), targetEffectorPos[j])
+			population[i].setCost(cost)
 
 
-def crossover(newPopulation, parameters1, parameters2):
-	#average a random parameter and append the new thing to the population array.
-	indiv = individual()
-	newParameters = []
-	for i in range(len(parameters1)):
-		newParameters.append( (parameters1[i]+parameters2[i]) /2)
-	indiv.setParameters(newParameters)
-
-	newPopulation.append(indiv)
-
-def mutateIndiv(indiv):
-	parameters = mutate(indiv.getParameters())
-	indiv.setParameters(parameters)
-
-def mutate (parameters):
-	scaleFactor = 1
-	randomInt = random.randint(0,len(parameters)-1)
-	randomVal = random.random() * scaleFactor - 0.5 * scaleFactor
-	parameters[randomInt] = parameters[randomInt] + randomVal
-	return parameters
-
-def createPopulation(startParameters, size):
-	#creates an initial population that has some starting variability
-	population = []
-	for i in range(size):
-		parameters = list(startParameters)
+	def crossover(self, newPopulation, parameters1, parameters2):
+		#average a random parameter and append the new thing to the population array.
 		indiv = individual()
-		parameters = mutate(parameters)
+		newParameters = []
+		for i in range(len(parameters1)):
+			newParameters.append( (parameters1[i]+parameters2[i]) /2)
+		indiv.setParameters(newParameters)
+
+		newPopulation.append(indiv)
+
+	def mutateIndiv(self, indiv):
+		parameters = self.mutate(indiv.getParameters())
 		indiv.setParameters(parameters)
-		population.append(indiv)
-	return population
 
-def geneticSelection(population, targetEffectorPos, travelDist):
-	selectionThreshold = 0.6
-	rejectionThreshold = 0.2
-	mutationChance     = 0.5
+	def mutate (self, parameters):
+		scaleFactor = 1
+		randomInt = random.randint(0,len(parameters)-1)
+		randomVal = random.random() * scaleFactor - 0.5 * scaleFactor
+		parameters[randomInt] = parameters[randomInt] + randomVal
+		return parameters
 
-	for i in range(30000):
-		#print "new Iteration"
-		updateCosts(population, targetEffectorPos, travelDist)
+	def createPopulation(self, startParameters, size):
+		#creates an initial population that has some starting variability
+		population = []
+		for i in range(size):
+			parameters = list(startParameters)
+			indiv = individual()
+			parameters = self.mutate(parameters)
+			indiv.setParameters(parameters)
+			population.append(indiv)
+		return population
 
-		population = sorted(population, key=lambda individual: individual.cost)
+	def geneticSelection(self, population, targetEffectorPos, travelDist, printer):
+		selectionThreshold = 0.6
+		rejectionThreshold = 0.2
+		mutationChance     = 0.5
+		#timeSinceLastExtinction = 0
 
-		#bottom third: deleted
-		#middle third: kept
-		#top third: makes children, better score = more children
-		#hardcode population size for now, eventually have it able to support arbitrary population sizes
+		for i in range(1000):
+			#print "new Iteration"
+			self.updateCosts(population, targetEffectorPos, travelDist, printer)
 
-		#hardcoded values: 
-		#0&1: 2 children
-		#2&3: 1 child
-		#4,5,6: keep
-		#7,8,9 delete
+			population = sorted(population, key=lambda individual: individual.cost)
 
-		population.remove(population[9])
-		population.remove(population[8])
-		population.remove(population[7])
-		
-		crossover(population, population[0].getParameters(), population[1].getParameters())
-		crossover(population, population[0].getParameters(), population[1].getParameters())
-		crossover(population, population[2].getParameters(), population[3].getParameters())
+			#bottom third: deleted
+			#middle third: kept
+			#top third: makes children, better score = more children
+			#hardcode population size for now, eventually have it able to support arbitrary population sizes
 
-		
-		#have a chance of mutating everything
-		for indiv in population:
-			if (random.random() < mutationChance):
-				mutateIndiv(indiv)
-		
-	return population
+			#hardcoded values: 
+			#0&1: 2 children
+			#2&3: 1 child
+			#4,5,6: keep
+			#7,8,9 delete
 
-targetParameters = 	[
-						-86.6,  86,   0,		#lower x
-						-50,   -50, 100,		#lower y
+			population.remove(population[9])
+			population.remove(population[8])
+			population.remove(population[7])
+			
+			self.crossover(population, population[0].getParameters(), population[1].getParameters())
+			self.crossover(population, population[0].getParameters(), population[1].getParameters())
+			self.crossover(population, population[2].getParameters(), population[3].getParameters())
 
-						-86.6,  86,   0,	    #upper x
-						-50,   -50, 100,		#upper y
-					]
+			
+			#have a chance of mutating everything
+			for indiv in population:
+				if (random.random() < mutationChance):
+					self.mutateIndiv(indiv)
 
-StartParameters = 	[
-						-85.6,  86,   0,	    #lower x
-						-50,   -50, 100,		#lower y
-
-						-86.6,  86,   0,	    #upper x
-						-50,   -50, 100,		#upper y
-					]
-
-#the set of carriage locations to generate the test points
-travelDist = [
-	[49.999, 50, 50],
-	[30, 50, 50],
-	[50, 30, 50],
-	[49.999, 50, 30],
-
-	[10, 50, 50],
-	[50, 10, 50],
-	[49.999, 50, 10],
-
-	[30.001, 30, 50],
-	[50, 30, 30],
-	[30, 50, 30],
-
-	[10.001, 10, 50],
-	[50, 10, 10],
-	[10, 50, 10],
-	]
-
-printer = PrinterModel.PrinterModel()
-printer.setParameters(targetParameters)
+			'''
+			timeSinceLastExtinction+=1
+			#extinction event
+			if (timeSinceLastExtinction == 100):
+				population = self.createPopulation(population[0].getParameters(), 10)
+				timeSinceLastExtinction = 0
+			'''
+			
+		return population
 
 
-targetEffectorPos = []
-for i in travelDist:
-	printer.setParameters(i)
-	targetEffectorPos.append(printer.getEffectorPosition())
+	def run(self):
+		targetParameters = 	[
+								-86.6,  86,   0,		#lower x
+								-50,   -50, 100,		#lower y
 
-population = createPopulation(StartParameters, 10)
+								-86.6,  86,   0,	    #upper x
+								-50,   -50, 100,		#upper y
+							]
 
-population = geneticSelection(population, targetEffectorPos, travelDist)
+		StartParameters = 	[
+								-85.6,  86,   0,	    #lower x
+								-50,   -50, 100,		#lower y
 
-for i in population:
-	print i.getParameters()
+								-86.6,  86,   0,	    #upper x
+								-50,   -50, 100,		#upper y
+							]
+
+		#the set of carriage locations to generate the test points
+		travelDist = [
+			[49.999, 50, 50],
+			[30, 50, 50],
+			[50, 30, 50],
+			[49.999, 50, 30],
+
+			[10, 50, 50],
+			[50, 10, 50],
+			[49.999, 50, 10],
+
+			[30.001, 30, 50],
+			[50, 30, 30],
+			[30, 50, 30],
+
+			[10.001, 10, 50],
+			[50, 10, 10],
+			[10, 50, 10],
+			]
+
+		printer = PrinterModel.PrinterModel()
+		printer.setParameters(targetParameters)
+
+
+		targetEffectorPos = []
+		for i in travelDist:
+			printer.setParameters(i)
+			targetEffectorPos.append( printer.getEffectorPosition()[2] )	#only get the z value
+
+		population = self.createPopulation(StartParameters, 10)
+
+		population = self.geneticSelection(population, targetEffectorPos, travelDist, printer)
+
+		for i in population:
+			print i.getParameters()
+
