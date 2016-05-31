4ac27cbdf1fd1b210dec13b625ff6471f3368eb2
malcolmwilliams <malcolmnwilliams@gmail.com>
2016-05-31 19:46:57 -0400

    minor bog fix

--- timeProfile.py
+++ timeProfile.py
@@ -2,5 +2,13 @@
 import GeneticOptimization
 import cProfile
 
+targetParameters = 	[
+						-86.6,  86,   0,		#lower x
+						-50,   -50, 100,		#lower y
+
+						-86.6,  86,   0,	    #upper x
+						-50,   -50, 100,		#upper y
+					]
+
 gOpt = GeneticOptimization.GeneticOptimization(100, 30, 0.5, 1, 100, 1)
-cProfile.run('gOpt.run()')
+cProfile.run('gOpt.run(targetParameters)')
